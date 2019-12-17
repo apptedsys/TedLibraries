aa735e47feb0e21135edf0c7da3237d9ba3546f0
apptedsys <53815843+apptedsys@users.noreply.github.com>
2019-12-17 11:31:30 +0530

    Second Commit
    Tested Module it was a greate success!

--- app/src/main/java/in/tedsys/tedlibraries/MainActivity.java
+++ app/src/main/java/in/tedsys/tedlibraries/MainActivity.java
@@ -1,14 +1,46 @@
 package in.tedsys.tedlibraries;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.json.JSONArray;
 
-public class MainActivity extends AppCompatActivity {
+import in.tedsys.tedutils.ResponseListener;
+import in.tedsys.tedutils.TedRequest;
+
+public class MainActivity extends AppCompatActivity implements ResponseListener {
+
+    private static final int REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_test).setOnClickListener(v -> Test());
+    }
+
+    private void Test() {
+        loadData();
+    }
+
+    private void loadData() {
+        TedRequest.get(this)
+                .url("http://192.168.0.116/ServizioService/TabService.ashx/GetTableList")
+                //.params(getParams())
+                .listener(this)
+                .requestCode(REQUEST_CODE)
+                .send();
+    }
+
+    @Override
+    public void onResponseSuccess(int RequestCode, JSONArray result) {
+        Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResponseError(int RequestCode, int ErrorCode, String Error) {
+        Toast.makeText(this, "Oops! " + Error, Toast.LENGTH_SHORT).show();
     }
 }
